c94f129defc8e9179aa71fec731c2b874b3d032d
tony20154689-ui <tony20154689@users.noreply.github.com>
2026-04-26 09:42:31 -0600

    Force memory-only Firestore cache + no-cache headers on index.html

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, memoryLocalCache } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -14,5 +14,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, { localCache: memoryLocalCache() });
 export const storage = getStorage(app);
